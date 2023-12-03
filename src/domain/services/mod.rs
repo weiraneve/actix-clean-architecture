7d99d3d5c2cacaa0af62cb3f31171ab36d61e435
weiraneve <907221539@qq.com>
2023-12-03 14:30:01 +0800

    feat: add app state service

--- src/domain/services/mod.rs
+++ src/domain/services/mod.rs
@@ -1,2 +1,11 @@
+use std::sync::Arc;
+use crate::domain::services::pick::PickService;
+use crate::domain::services::reset::ResetService;
+
 pub mod pick;
 pub mod reset;
+
+pub struct Service {
+    pub pick: Arc<dyn PickService>,
+    pub reset: Arc<dyn ResetService>,
+}
