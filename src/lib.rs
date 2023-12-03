86861301c701db65ee9eeadc17918a1fbec6ce4f
weiraneve <907221539@qq.com>
2023-12-03 11:22:25 +0800

    feat: init file architecture

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod api;
+pub mod domain;
+pub mod services;
+pub mod infrastructure;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(|| async { "Hello, world!" }))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
